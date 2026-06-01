4024b4a0e2d85dc129491116d99bd30730adce96
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-06-01 19:38:53 +0100

    fix: use setupFilesAfterEnv (correct Jest config key for BigInt support)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'json-summary'],
